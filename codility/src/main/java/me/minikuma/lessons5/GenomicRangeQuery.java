6d81410a09dedcfcf08a139c726523d5c332eda5
minikuma <minikuma@naver.com>
2021-11-05 00:07:31 +0900

    lessons 5 GenomicRangeQuery O(N * M)

--- codility/src/main/java/me/minikuma/lessons5/GenomicRangeQuery.java
+++ codility/src/main/java/me/minikuma/lessons5/GenomicRangeQuery.java
@@ -4,43 +4,55 @@ import java.util.*;
 
 public class GenomicRangeQuery {
     public int[] solution(String S, int[] P, int[] Q) {
-        Map<Character, Integer> maps = new HashMap<>();
-        maps.put('A', 1);
-        maps.put('C', 2);
-        maps.put('G', 3);
-        maps.put('T', 4);
-
-        List<List<Character>> arr = new ArrayList<>();
     /*
         P[0] = 2    Q[0] = 4
         P[1] = 5    Q[1] = 5
         P[2] = 0    Q[2] = 6
      */
-        for (int i = 0; i < P.length; i++) {
-            System.out.println("(" + P[i] + "," + Q[i] + ")");
-            List<Character> t = new ArrayList<>();
-            for (int j = P[i]; j <= Q[i]; j++) {
-                if (!t.contains(S.charAt(j))) {
-                    t.add(S.charAt(j));
+        int M = P.length;
+        int[] result = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            String str = S.substring(P[i], Q[i] + 1);
+            if (str.contains("A")) {
+                result[i] = 1;
+            } else {
+                if (str.contains("C")) {
+                    result[i] = 2;
+                } else {
+                    if (str.contains("G")) {
+                        result[i] = 3;
+                    } else {
+                        result[i] = 4;
+                    }
                 }
             }
-            arr.add(t);
         }
-        arr.forEach(System.out::println);
-
-        int[] result = new int[arr.size()];
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = 0; j < arr.get(i).size(); j++) {
-                int a = maps.get(arr.get(i).get(j));
-                min = Math.min(min, a);
-            }
-            result[i] = min;
-            min = Integer.MAX_VALUE;
-        }
-
         return result;
+//
+//        for (int i = 0; i < P.length; i++) {
+//            System.out.println("(" + P[i] + "," + Q[i] + ")");
+//            List<Character> t = new ArrayList<>();
+//            for (int j = P[i]; j <= Q[i]; j++) {
+//                if (!t.contains(S.charAt(j))) {
+//                    t.add(S.charAt(j));
+//                }
+//            }
+//            arr.add(t);
+//        }
+//        arr.forEach(System.out::println);
+//
+//        int[] result = new int[arr.size()];
+//        int min = Integer.MAX_VALUE;
+//
+//        for (int i = 0; i < arr.size(); i++) {
+//            for (int j = 0; j < arr.get(i).size(); j++) {
+//                int a = maps.get(arr.get(i).get(j));
+//                min = Math.min(min, a);
+//            }
+//            result[i] = min;
+//            min = Integer.MAX_VALUE;
+//        }
     }
 
     public static void main(String[] args) {
