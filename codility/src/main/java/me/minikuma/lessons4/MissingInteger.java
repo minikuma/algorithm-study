6a62aa4b06fe7fdeb1555d034275bea07f858ac6
minikuma <minikuma@naver.com>
2021-11-15 22:51:27 +0900

    lessons 4 Missing Integer - Hash Set

--- codility/src/main/java/me/minikuma/lessons4/MissingInteger.java
+++ codility/src/main/java/me/minikuma/lessons4/MissingInteger.java
@@ -1,36 +1,38 @@
 package me.minikuma.lessons4;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MissingInteger {
     /*
-    *   음수인 경우 고려 해야 함
+    *   등장하지 않는 수는 Hash 구조를 생각해 볼것.
+    *   중복을 허용하지 않는다.
     * */
     public int solution(int[] A) {
-        Arrays.sort(A);
-        int missingNum = 1;
-        // 1, 1, 2, 3, 4, 6
-        // -1 -2, -3
-        for (int i = 0; i < A.length - 1; i++) {
-            if (A[i + 1] - A[i] >= 2) {
-                missingNum = A[i] + 1;
-            }
+        Set<Integer> sets = new HashSet<>();
+
+        for (int i = 0; i < A.length; i++) {
+            sets.add(A[i]);
         }
 
-        return missingNum;
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            if (!sets.contains(i)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         int[] a = {1, 3, 6, 4, 1, 2};
-        int[] b = {-1, -2 , -3};
-        int[] c = {-1, -2 , 1, 3};
+        int[] b = {-1, -2, -3};
+        int[] c = {-1, -2 ,0, 1, 2};
         int[] d = {1, 2, 3, 4};
 
-
         MissingInteger m = new MissingInteger();
-        System.out.println("res = " + m.solution(a));
-        System.out.println("res = " + m.solution(b));
-        System.out.println("res = " + m.solution(c));
-        System.out.println("res = " + m.solution(d));
+        System.out.println("a_res = " + m.solution(a));
+        System.out.println("b_res = " + m.solution(b));
+        System.out.println("c_res = " + m.solution(c));
+        System.out.println("d_res = " + m.solution(d));
     }
 }
