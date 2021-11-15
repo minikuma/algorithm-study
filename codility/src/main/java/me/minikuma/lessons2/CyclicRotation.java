26dbb8a9555f12c183ed2a06e9ec019ec7bae18a
minikuma <minikuma@naver.com>
2021-11-15 23:26:23 +0900

    lessons 2 Cyclic Rotation Array Shift - codition 추가

--- codility/src/main/java/me/minikuma/lessons2/CyclicRotation.java
+++ codility/src/main/java/me/minikuma/lessons2/CyclicRotation.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
  */
 public class CyclicRotation {
     public int[] solution(int[] A, int K) {
+        if (A.length == 0) return A; // 고려사항
         int size = A.length;
         for (int i = 0; i < K; i++) {
             int temp = A[size - 1];
@@ -20,8 +21,9 @@ public class CyclicRotation {
 
     public static void main(String[] args) {
         int[] a = {3, 8, 9, 7, 6};
-        int k = 3;
+        int[] b = {0, 0, 0, 0};
+        int k = 1;
         CyclicRotation c = new CyclicRotation();
-        System.out.println(Arrays.toString(c.solution(a, k)));
+        System.out.println(Arrays.toString(c.solution(b, k)));
     }
 }
