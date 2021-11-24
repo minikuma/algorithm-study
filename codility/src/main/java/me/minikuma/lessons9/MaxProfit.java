381c0d82045ae67c44e85d99d46912e6b2f6c87c
minikuma <minikuma@naver.com>
2021-11-24 23:03:52 +0900

    MaxProfit - O(N) 부분합

--- codility/src/main/java/me/minikuma/lessons9/MaxProfit.java
+++ codility/src/main/java/me/minikuma/lessons9/MaxProfit.java
@@ -4,33 +4,53 @@ package me.minikuma.lessons9;
  * 정확도: 100%
  * 효율성: 25%
  * 총점: 66% O(N**2)
+ * 부분 합 문제
  */
 
 public class MaxProfit {
     public int solution(int[] A) {
-        int max = 0;
+        int res = 0;
         // Brute Force
         for (int i = 0; i < A.length; i++) {
             for(int j = i + 1; j < A.length; j++) {
                 int temp = A[j] - A[i];
-                System.out.println("temp = " + temp);
-                if (temp > max) {
-                    max = temp;
+                if (temp > res) {
+                    res = temp;
                 }
             }
         }
 
+        int min = A[0]; //5
+        int profit = A[1] - A[0]; // 1
+        //N is an integer within the range [0..400,000]
+        // 배열이 빈 배열일수도 있다.
 
-        return max;
+        if (A.length < 2) {
+            return 0;
+        }
+
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < min) {
+                min = A[i];
+            } else {
+                profit = Math.max(profit, A[i] - min);
+            }
+        }
+
+        if (profit < 0) {
+            return 0;
+        }
+
+        return profit;
     }
 
     public static void main(String[] args) {
-//        int[] a1 = {23171, 21011, 21123, 21366, 21013, 21367};
+        int[] a1 = {23171, 21011, 21123, 21366, 21013, 21367};
         int[] a2 = {5,4,3,2,1};
         MaxProfit m = new MaxProfit();
-//        int res1 = m.solution(a1);
+        int res1 = m.solution(a1);
         int res2 = m.solution(a2);
-//        System.out.println("res1 = " + res1);
+        System.out.println("res1 = " + res1);
         System.out.println("res2 = " + res2);
     }
 }
