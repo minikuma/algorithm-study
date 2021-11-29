4e3686b1c3b2720a4614b1ce284e277a0aa34616
minikuma <minikuma@naver.com>
2021-11-29 15:13:42 +0900

    Flags - difficult-re-try

--- codility/src/main/java/me/minikuma/lessons10/Flags.java
+++ codility/src/main/java/me/minikuma/lessons10/Flags.java
@@ -1,33 +1,73 @@
 package me.minikuma.lessons10;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 어려운 문제: 구글링을 통해 겨우 이해, 다시 봐야 함
+ */
 public class Flags {
     public int solution(int[] A) {
-        if (A.length < 4) return 1;
+        int len = A.length;
 
-        // peak point를 먼저 계산
+        // peak point 를 먼저 계산
         // 0 < P < N -1, A[P - 1] < A[P] > A[P + 1] = A[P]가 peak point
-        List<Integer> lists = new ArrayList<>();
+        List<Integer> peaks = new ArrayList<>();
         // A 배열 스캔하면서 Peak Point 구한다
         // O (N)
-        for (int i = 1; i < A.length - 1; i++) {
+        for (int i = 1; i < len - 1; i++) {
             if (A[i - 1] < A[i] && A[i] > A[i + 1]) {
-                lists.add(i);
+                peaks.add(i);
             }
         }
 
+        int pSize = peaks.size();
+
+        if (pSize < 2) {
+            return pSize;
+        }
+
+        System.out.println(peaks);
+
         // flags 정의
-        System.out.println(lists);
-        return -1;
+        int sf = 1;
+        int ef = pSize;
+        int max = 0;
+
+        while (sf <= ef) {
+            int flag = (sf + ef) / 2;
+            boolean check = false;
+            int used = 0;
+            int prevPeak = peaks.get(0);
+
+            for (int i = 0; i < pSize; i++) {
+                if (peaks.get(i) >= prevPeak) {
+                    used++;
+                    prevPeak = peaks.get(i) + flag;
+                    if (used == flag) {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+
+            if (check) {
+                max = flag;
+                sf = flag + 1;
+            } else {
+                ef = flag - 1;
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         int[] p1 = {1,5,3,4,3,4,1,2,3,4,6,2};
+        int[] p2 = {1, 3, 2};
         Flags f = new Flags();
         int res1 = f.solution(p1);
+        int res2 = f.solution(p2);
         System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
     }
 }
