38375a9aa6fb0f1a9654c222035c155a9cfe8bba
minikuma <minikuma@naver.com>
2021-11-30 00:31:05 +0900

    lessons11 CountNonDivisible - (*)

--- codility/src/main/java/me/minikuma/lessons11/CountNonDivisible.java
+++ codility/src/main/java/me/minikuma/lessons11/CountNonDivisible.java
@@ -6,6 +6,7 @@ public class CountNonDivisible {
     public int[] solution(int[] A) {
         // O (N ** 2)
         int[] count = new int[2 * A.length + 1];
+        int[] result = new int[A.length];
         for (int i = 0; i < A.length; i++) {
             count[A[i]]++;
         }
@@ -14,8 +15,23 @@ public class CountNonDivisible {
         // [0, 1, 1, 2, 0, 0, 1, 0, 0, 0, 0]
 
         System.out.println(Arrays.toString(count));
+        // 3,1,2,3,6
+        for (int i = 0; i < A.length; i++) {
+            int divisor = 0;
+            int sq = (int) Math.sqrt(A[i]);
+            System.out.println("sq = " + sq);
+            for (int j = 1; j <= sq; j++) { //1
+                if (A[i] % j == 0) {
+                    divisor += count[j];
+                    if (A[i] / j != j) {
+                        divisor += count[A[i] / j];
+                    }
+                }
+            }
+            result[i] = A.length - divisor;
+        }
 
-        return null;
+        return result;
     }
 
     public static void main(String[] args) {
