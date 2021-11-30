eb543407285837aefa6275db58b3c2b5270ede71
minikuma <minikuma@naver.com>
2021-11-30 13:28:09 +0900

    ChocolatesByNumbers - 재귀 (최대공약수)

--- codility/src/main/java/me/minikuma/lessons12/ChocolatesByNumbers.java
+++ codility/src/main/java/me/minikuma/lessons12/ChocolatesByNumbers.java
@@ -1,24 +1,35 @@
 package me.minikuma.lessons12;
 
 public class ChocolatesByNumbers {
+
     public int solution(int N, int M) {
-        // (X + M) %  N
-        int count = 0;
-        int X = 0;
-        do {
-            X = (X + M) % N;
-            count++;
-        } while (X != 0);
-        return count;
+        // (X + M) / N == 0 이 되는 값 10 / 4
+        return N / gcd(N, M);
+    }
+    // 최대 공약수
+    private int gcd(int n, int m) {
+        if (n % m == 0) {
+            return m;
+        } else {
+            return gcd(m, n % m);
+        }
     }
 
 
     public static void main(String[] args) {
         int n1 = 10;
         int m1 = 4;
+        int n2 = 1000000000;
+        int m2= 1;
+        int n3 = 415633212;
+        int m3 = 234332;
 
         ChocolatesByNumbers cb = new ChocolatesByNumbers();
         int res1 = cb.solution(n1, m1);
+        int res2 = cb.solution(n2, m2);
+        int res3 = cb.solution(n3, m3);
         System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+        System.out.println("res3 = " + res3);
     }
 }
