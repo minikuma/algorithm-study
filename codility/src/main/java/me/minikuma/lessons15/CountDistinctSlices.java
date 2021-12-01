d08963092f505369f2a6024dcdfcb43cffa5f1af
minikuma <minikuma@naver.com>
2021-12-02 00:05:39 +0900

    lessons15 CountDistinctSlices - two pointer, memorization

--- codility/src/main/java/me/minikuma/lessons15/CountDistinctSlices.java
+++ codility/src/main/java/me/minikuma/lessons15/CountDistinctSlices.java
@@ -1,28 +1,30 @@
 package me.minikuma.lessons15;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
+/*
+    다시 봐야 함 포인터를 2개 사용, 체크 배열을 별도로 가져감(*)
+ */
 public class CountDistinctSlices {
     public int solution(int M, int[] A) {
-        // 배열 스캔
-        // M 보다 작은 값 확인
-        // 중복 값이 없는 지 확인
-        Set<Integer> sets = new HashSet<>();
+        // 배열 요소 Unique 체크 용
+        boolean[] check = new boolean[M + 1];
+        int count = 0;
+        int front = 0;
+        int end = 0;
 
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] <= M) {
-                if (!sets.contains(A[i])) {
-                    sets.add(A[i]);
-                }
+        while (front < A.length && end < A.length) {
+            if (check[A[end]]) {
+                check[A[front]] = false;
+                front++;
+            } else {
+                check[A[end]] = true;
+                count += (end - front + 1);
+                end++;
             }
-
         }
 
-
-        return 0;
+        if (count >= 1000000000) {
+            return 1000000000;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
