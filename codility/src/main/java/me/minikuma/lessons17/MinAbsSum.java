125de8524eda120437a298ede4b7911ba45d10be
minikuma <minikuma@naver.com>
2021-12-08 00:26:51 +0900

    lessons17-MinAbsSum difficult

--- codility/src/main/java/me/minikuma/lessons17/MinAbsSum.java
+++ codility/src/main/java/me/minikuma/lessons17/MinAbsSum.java
@@ -1,15 +1,66 @@
 package me.minikuma.lessons17;
 
+import java.util.Arrays;
+
 public class MinAbsSum {
     public int solution(int[] A) {
-        return -1;
+        int n = A.length;
+        if (n < 1) return 0;
+
+        int[] abs = new int[n];
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(A[i]);
+            abs[i] = val;
+            max = Math.max(max, val);
+            sum += val;
+        }
+
+        int[] count = new int[max + 1];
+        for (int i = 0; i < n; i++) {
+            int val = abs[i];
+            count[val]++;
+        }
+
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= max; i++) {
+            int cnt = count[i];
+            if (cnt > 0) {
+                for (int j = 0; j <= sum; j++) {
+                    if (dp[j] >= 0) {
+                        dp[j] = cnt;
+                    } else if (j >= i && dp[j - i] > 0) {
+                        dp[j] = dp[j - i] - 1;
+                    }
+                 }
+            }
+        }
+
+        int res = sum;
+        for (int i = 0; i < sum / 2 + 1; i++) {
+            if (dp[i] >= 0) {
+                res = Math.min(res, sum - 2 * i);
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] a1 = {};
+        int[] a1 = {1, 5, 2, -2};
+        int[] a2 = {2, 2, 1};
+        int[] a3 = {3, 3, 3, 4, 5};
         MinAbsSum m = new MinAbsSum();
         int res1 = m.solution(a1);
+        int res2 = m.solution(a2);
+        int res3 = m.solution(a3);
         System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+        System.out.println("res3 = " + res3);
     }
 }
 
